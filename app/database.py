efe94c6a73871e75d07e5aba8fd458060dc1a67e
httpsabhi <abhaykumar714371@gmail.com>
2024-11-25 22:28:07 +0530

    connection fix

--- app/database.py
+++ app/database.py
@@ -1,20 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import OperationalError
 from .config import settings
+
 
 DATABASE_URI = settings.DATABASE_URI
 
 if not DATABASE_URI:
     raise ValueError("DATABASE_URI not found")
 
-engine = create_engine(
-    DATABASE_URI,
-    pool_pre_ping=True,
-    connect_args={"sslmode": "require"}, 
-)
-
+engine = create_engine(DATABASE_URI)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base = declarative_base()
 
@@ -22,10 +17,5 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
-    except OperationalError as e:
-        print("Database connection lost. Attempting to reconnect...")
-        db.rollback()  
-        db.close() 
-        raise e
     finally:
         db.close()
